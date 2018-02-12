99e324f65ba6a39ac2b0d556f985365a7600c302
Metz <tlm2702@email.vccs.edu>
2018-02-12 18:51:04 -0500

    Corrected gitignore

--- notes/ch7.py
+++ notes/ch7.py
@@ -1,5 +1,12 @@
-list_1 = [4, 7, 2, -9, 6]
-list_2 = [1, 2, 3, 4, 5, 6]
-big_list = list_1 + list_2
-print(big_list)
-print(len(big_list))
+# list_1 = [4, 7, 2, -9, 6]
+# list_2 = [1, 2, 3, 4, 5, 6]
+# big_list = list_1 + list_2
+# print(big_list)
+# print(len(big_list))
+
+# Methods come with the object.  It is a function called in a special way, the dot call.
+# e.g. my_str.replace() is a method.  my_list.append() is a method.
+
+my_str = "Tammy is cool"
+new_str = my_str.replace('m','z')
+print (my_str, new_str)
