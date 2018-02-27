34ed9318738251f9caa1992f77d73b588e26e3d5
Tammy Metz <tammy@tammymetz.com>
2018-02-26 19:48:17 -0500

    Practice from Feb 26

--- In class practice/ch8practice1.py
+++ In class practice/ch8practice1.py
@@ -30,11 +30,12 @@ def main(): # main is "indent level 0"
     """Get a line_str from the file, print the final grade nicely"""
     file_name = input('Open what file:')
     grade_file = open(file_name,'r')
-    print('{:>13s} {:>15s}'.format('Name', 'Grade'))
+    print('{:>13s} {:>16s}'.format('Name', 'Grade'))
     print('-'*30)
     for line_str in grade_file:
         name_str, score_list = parse_line(line_str)
         grade_float = weighted_grade(score_list, (.5, .4, .1))
         print('{:>15s} {:14.2f}'.format(name_str, grade_float))
 
-main()
+if __name__ ==  "__main__":
+    main()
