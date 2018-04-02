96f952cbf83707b7272aecac31293e8d613ad551
Tammy Metz <tammy@tammymetz.com>
2018-04-02 19:37:04 -0400

    Work from April 2 2018

---
+++ In class practice/ch11practice.py
@@ -0,0 +1,31 @@
+import math
+
+class Point(object):
+    def __init__(self, x_param = 0.0, y_param = 0.0):
+        self.x = x_param
+        self.y = y_param
+
+    def distance(self, param_pt):
+        x_diff = self.x - param_pt.x
+        y_diff = self.y - param_pt.y
+        return math.sqrt(x_diff**2 + y_diff**2)
+
+    def sum(self, param_pt):
+        return Point(self.x + param_pt.x, self.y + param_pt.y)
+
+    def __str__(self):
+        return("({:.2f}, {:.2f})".format(self.x, self.y))
+
+def main():
+    pt1_inst = Point(5.0, 9.0)
+    pt2_inst = Point(3.0, 3.0)
+    print(pt1_inst)
+    print(pt2_inst)
+    print("Sum: ", pt2_inst.sum(pt1_inst))
+    print("Distance: ", pt2_inst.distance(pt1_inst))
+
+    print("Sum: ", pt1_inst.sum(pt2_inst))
+    print("Distance: ", pt1_inst.distance(pt2_inst))
+
+if __name__ == "__main__":
+    main()
--- notes/ch11.py
+++ notes/ch11.py
@@ -1,6 +1,6 @@
-class Student(object):  # What goes in the parens is all the classes your class inherits (e.g. Python's object) [parent objects]
-    def __init__(self, first='', last='', id=0):
-        self.first_name_str = "test"
+# class Student(object):  # What goes in the parens is all the classes your class inherits (e.g. Python's object) [parent objects]
+#     def __init__(self, first='', last='', id=0):
+#         self.first_name_str = "test"
 
 # Usually class names start with a capital letter
 # All Python data types are classes
@@ -13,17 +13,44 @@ class Student(object):  # What goes in the parens is all the classes your class 
 
 # The dir() function lists all the attributes of a class, including all that it inherits
 
-class MyClass(object):
-    def __init__(self, last, first):
-        self.last_name = last
-        self.first_name = first
-    def pretty_print(self):
-        print("My name is {} {}".format(self.first_name, self.last_name))
+class Student(object):
 
-my_obj = MyClass("Metz", "Tammy")  # <-- constructor is MyClass("Metz", "Tammy")
-MyClass.my_attrib = "Hello"  #  You can do this too!!!
-print(dir(my_obj))
+    def __init__(self, first='', last='', id=0):
+        self.last_name_str = last
+        self.first_name_str = first
+        self.id_int = id
+        # self.__id_int = id would make id_int private
 
-my_obj.pretty_print()
-print(my_obj.__class__) # my_obj is an instance-of the MyClass class
-print(my_obj.__dict__) # the namespace
+    def update(self, first='', last='', id=0):
+        if first:
+            self.first_name_str = first
+        if last:
+            self.last_name_str = last
+        if id:
+            self.id_int = id
+
+    def __str__(self): # Converts the object to a string so we can print info about the object nicely
+        return "{} {}, ID: {}".format(self.first_name_str, self.last_name_str, self.id_int)
+
+def main():
+    tammy_obj = Student("Tammy", "Metz", 66666)
+    print(tammy_obj)
+    tammy_obj.update(first="Tamara")
+    print(tammy_obj)
+    print(dir(Student))
+    # print(_Student__id_int) would allow you to modify the private variable from outside the class (which you shouldn't)
+
+
+if __name__=="__main__":
+    main()
+
+    # def pretty_print(self):
+    #     print("My name is {} {}".format(self.first_name, self.last_name))
+
+# my_obj = MyClass("Metz", "Tammy")  # <-- constructor is MyClass("Metz", "Tammy")
+# MyClass.my_attrib = "Hello"  #  You can do this too!!!
+# print(dir(my_obj))
+#
+# my_obj.pretty_print()
+# print(my_obj.__class__) # my_obj is an instance-of the MyClass class
+# print(my_obj.__dict__) # the namespace
